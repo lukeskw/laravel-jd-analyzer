5a2b982e5bf35f776248f87a82bc8361e08aa88b
Lucas Porfirio Dev <lucasporfirioa@gmail.com>
2025-10-26 18:07:46 -0300

    feat: add PdfParserService to coordinate parsing in PdfParserService.php

---
+++ app/Services/PdfParserService.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Services;
+
+use App\Contracts\PdfParserInterface;
+
+class PdfParserService
+{
+    public function __construct(private PdfParserInterface $parser)
+    {
+    }
+
+    public function parse(string $path): string
+    {
+        return $this->parser->extractText($path);
+    }
+}
+
