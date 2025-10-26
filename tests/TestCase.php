9c187c84ea6d21c4a2b203bca2aebb6f4e87308e
Lucas Porfirio Dev <lucasporfirioa@gmail.com>
2025-10-26 18:07:46 -0300

    test: update base TestCase setup in tests/TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,23 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Config;
+use Mockery;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        Config::set('database.default', 'sqlite');
+        Config::set('database.connections.sqlite.database', ':memory:');
+    }
+
+    protected function tearDown(): void
+    {
+        Mockery::close();
+
+        parent::tearDown();
+    }
 }
