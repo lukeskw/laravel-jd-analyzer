e1b499d8d963f3fee46803151372403180c44b9c
Lucas Porfirio Dev <lucasporfirioa@gmail.com>
2025-10-26 18:07:46 -0300

    feat: add JobDescription model in JobDescription.php

---
+++ app/Models/JobDescription.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class JobDescription extends Model
+{
+    use HasFactory, HasUuids;
+
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'title',
+        'filename',
+        'stored_path',
+        'text',
+    ];
+
+    public function candidates(): HasMany
+    {
+        return $this->hasMany(Candidate::class);
+    }
+}
