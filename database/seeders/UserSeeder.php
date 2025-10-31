6351fa6c4a45d5484717697ca1fb7843ad91ca7d
Lucas Porfirio Dev <lucasporfirioa@gmail.com>
2025-10-31 02:02:40 -0300

    ci: adding sqlite file and adding migration on the container; adding password coming from env

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,16 +4,19 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
     public function run(): void
     {
+        $password = env('USER_SEEDER_PASSWORD', 'password');
+
         User::query()->updateOrCreate(
             ['email' => 'test@test.com'],
             [
                 'name' => 'Test User',
-                'password' => 'password',
+                'password' => Hash::make($password),
             ]
         );
     }
